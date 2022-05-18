4b5c03a7cecee8a669b8e2d97954956532f19334
HHuangYQ <1135782479@qq.com>
2022-05-18 21:18:43 +0800

    准备上传了

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -2,7 +2,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  
   entry: "./src/index.js",
   output: {
     filename: "index.[contenthash].js",
@@ -11,6 +10,35 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "pakho",
       template: "src/assets/index.html",
-    })
+    }),
   ],
+  module: {
+    rules: [
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use:['file-loader'],
+            },
+        {
+            test: /\.styl$/,
+            loader: ["style-loader", "css-loader", "stylus-loader"],
+          },
+      {
+        test: /\.less$/,
+        loader: ["style-loader", "css-loader", "less-loader"],
+      },
+        {
+            test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("dart-sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
